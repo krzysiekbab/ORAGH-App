5e8760d91832c73c2e262a5e16e028f9aec780b8
Krzysztof Babicki <krzysztofbabicki@Mac.lan>
2025-06-10 22:53:28 +0200

    Add registration

--- oragh_platform/oragh_platform/settings.py
+++ oragh_platform/oragh_platform/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'register',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +135,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LANGUAGE_CODE = 'pl' 
---
+++ oragh_platform/register/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+
+# Create your views here.
+def register(request):
+    """
+    Render the registration page.
+    """
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # user = form.save()
+            # username = form.cleaned_data.get("username")
+            # raw_password = form.cleaned_data.get("password1")
+            # user = authenticate(username=username, password=raw_password)
+            # login(request, user)
+            # return render(request, "registration/success.jinja", {"user": user})
+            return redirect("/home")
+        
+    form = UserCreationForm(request.POST or None)
+    return render(request, "register.jinja", {"form": form})
