39e90b4c6f04e03506e2e0762c3e0d494497ff5c
Krzysztof Babicki <krzysztofbabicki@Mac.lan>
2025-06-13 16:41:34 +0200

    Add istrument to user

---
+++ oragh_platform/register/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from main.models import INSTRUMENT_CHOICES
+
+class CustomUserCreationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label="Imię")
+    last_name = forms.CharField(max_length=30, required=True, label="Nazwisko")
+    username = forms.CharField(max_length=150, required=True, label="Nick")
+    email = forms.EmailField(required=True, label="Email")
+    instrument = forms.ChoiceField(choices=INSTRUMENT_CHOICES, required=True, label="Instrument")
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "username", "email", "password1", "password2", "instrument")
--- oragh_platform/register/views.py
+++ oragh_platform/register/views.py
@@ -1,22 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
+from .forms import CustomUserCreationForm
+from main.models import MusicianProfile
 
-# Create your views here.
 def register(request):
     """
     Render the registration page.
     """
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            # Add user to "musican" group
-            musican_group, created = Group.objects.get_or_create(name="musician")
-            user.groups.add(musican_group)
-            user.refresh_from_db()
+            user = form.save(commit=False)
+            user.first_name = form.cleaned_data["first_name"]
+            user.last_name = form.cleaned_data["last_name"]
+            user.email = form.cleaned_data["email"]
+            user.save()
+            # Add user to "musician" group
+            musician_group, created = Group.objects.get_or_create(name="musician")
+            user.groups.add(musician_group)
+            # Create MusicianProfile
+            instrument = form.cleaned_data["instrument"]
+            MusicianProfile.objects.create(user=user, instrument=instrument)
             return redirect("/login")
-        
-    form = UserCreationForm(request.POST or None)
+    else:
+        form = CustomUserCreationForm()
     return render(request, "register.jinja", {"form": form})
