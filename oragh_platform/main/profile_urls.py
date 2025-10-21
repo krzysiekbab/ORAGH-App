dfabcda22147ca1a16ca0aa72f699dfecdc2b82c
Krzysztof Babicki <krzysztofbabicki@MacBook-Pro-Krzysztof.local>
2025-10-21 15:51:00 +0200

    Remove old project

--- oragh_platform/main/profile_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import edit_profile, profile_overview
-
-urlpatterns = [
-    path('', profile_overview, name='profile'),
-    path('edit/', edit_profile, name='edit_profile'),
-]
